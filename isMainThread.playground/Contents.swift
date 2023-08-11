d461a2f1318a8d15ebbd3589764c6a3769718cf4
Shubhransh Gupta <shubhransh.gupta@gonuclei.com>
2023-08-11 20:28:09 +0530

    first commit

---
+++ isMainThread.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+func doSomething() {
+    concurrentQueue.asyncAfter(deadline: .now() + 3, execute: {
+        if Thread.isMainThread {
+            print("Executing on the main thread")
+        } else {
+            print("Executing on a concurrent thread")
+        }
+    })
+
+}
+
+// Run on the main thread
+
+// Create and run a concurrent thread
+let concurrentQueue = DispatchQueue(label: "com.example.concurrentQueue", attributes: .concurrent)
+concurrentQueue.sync {
+    doSomething()
+}
+print("123")
+for i in 0..<10 {
+    print("123")
+}
+
