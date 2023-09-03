e78ca1182ec951f627883d5cfc825e48d0e7ff5f
Shubhransh Gupta <shubhransh.gupta@gonuclei.com>
2023-09-03 16:16:59 +0530

    fileprivate and private

---
+++ AccessSpecifier.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+
+class Employee {
+    fileprivate var fname : String?
+    private var lname : String?
+    
+    init(_ f : String, _ l : String) {
+        self.fname = f
+        self.lname = l
+    }
+    
+    fileprivate func getFname() -> String? {
+        return self.fname
+    }
+    
+    private func getLname() -> String? {
+        return self.lname
+    }
+}
+
+extension Employee {
+    internal func getEmployeeFullName() {
+        print(self.fname! + self.lname!)
+    }
+}
+
+class Person : Employee {
+    
+    override init(_ name : String, _ nameTwo : String) {
+        super.init(name, nameTwo)
+    }
+    
+    fileprivate func printEmployee() -> String? {
+        return self.fname! //+ self.lname!
+    }
+    
+    override func getFname() -> String? {
+        return (self.fname!)
+    }
+    
+//    override func getLname() -> String? {
+//        return (self.lname!)
+//    }
+    
+}
+
+let per = Person("Ram", "Singh")
+print(per.printEmployee())
+per.getEmployeeFullName()
